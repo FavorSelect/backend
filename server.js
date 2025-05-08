1a69e57a985e98a3f7b7533a266fc735639078a6
Satish Singh <i.sksingh113@gmail.com>
2025-05-08 15:56:33 +0530

    42th commit

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ const reviewLikeRoute = require('./routes/reviewLikeRoute/reviewLikeRoute')
 const app = express();
 const PORT = process.env.PORT || 8001;
 
+app.use(cors({
+  origin:process.env.FRONTEND_URL,  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+  allowedHeaders: ['Content-Type', 'Authorization'],  
+  credentials:true                                                 
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
