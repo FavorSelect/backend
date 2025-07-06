cbe441b3a9963193cf8cfb8db1f477f1d736c4ae
Satish Singh <i.sksingh113@gmail.com>
2025-07-06 23:07:02 +0530

    token issue

--- authService/setTokenCookie.js
+++ authService/setTokenCookie.js
@@ -1,18 +1,17 @@
 const { serialize } = require("cookie");
 
-const setTokenCookie = (res, token) => {
+const setTokenCookie = (res, token, middlewareToken) => {
   res.setHeader("Set-Cookie", [
-    // HttpOnly cookie for backend security
+    // Secure, HttpOnly cookie
     serialize("token", token, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
       path: "/",
-      maxAge: 60 * 60 * 24 * 360,
+      maxAge: 60 * 60 * 24 * 360, 
     }),
-
-    // Accessible cookie for middleware
-    serialize("token_middleware", token, {
+    // Non-HttpOnly for middleware
+    serialize("token_middleware", middlewareToken, {
       httpOnly: false,
       secure: true,
       sameSite: "none",
