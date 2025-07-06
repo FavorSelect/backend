1bf1d122e0f190af2988f81976f5edfb94443c1e
Satish Singh <i.sksingh113@gmail.com>
2025-07-06 22:32:15 +0530

    token issue fixed

--- authService/clearCookie.js
+++ authService/clearCookie.js
@@ -1,11 +1,22 @@
 const { serialize } = require("cookie");
+
 const clearTokenCookie = (res) => {
-  res.setHeader("Set-Cookie", serialize("token", "", {
-    httpOnly: true,
-    secure: true,       
-    sameSite: "none",      
-    path: "/",            
-    expires: new Date(0), 
-  }));
+  res.setHeader("Set-Cookie", [
+    serialize("token", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      expires: new Date(0),
+    }),
+    serialize("token_middleware", "", {
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      expires: new Date(0),
+    }),
+  ]);
 };
+
 module.exports = clearTokenCookie;
--- authService/setTokenCookie.js
+++ authService/setTokenCookie.js
@@ -1,16 +1,25 @@
 const { serialize } = require("cookie");
 
 const setTokenCookie = (res, token) => {
-  res.setHeader(
-    "Set-Cookie",
+  res.setHeader("Set-Cookie", [
+    // HttpOnly cookie for backend security
     serialize("token", token, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
       path: "/",
-      maxAge: 60 * 60 * 24 * 360, 
-    })
-  );
+      maxAge: 60 * 60 * 24 * 360,
+    }),
+
+    // Accessible cookie for middleware
+    serialize("token_middleware", token, {
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 360,
+    }),
+  ]);
 };
 
 module.exports = setTokenCookie;
